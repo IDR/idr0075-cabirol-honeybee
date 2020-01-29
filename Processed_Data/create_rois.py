d7fa68f9b1838dca89b23ed9e21f3dffb74f7036
Dominik Lindner <d.lindner@dundee.ac.uk>
2020-01-29 09:30:17 +0000

    Add roi csv files and script

---
+++ Processed_Data/create_rois.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import pandas
+import omero
+from omero.gateway import BlitzGateway
+from omero.rtypes import rdouble, rint
+
+z_size = 0.5
+z_planes = 101
+
+x_size = 0.3438
+x_max = 512
+
+y_size = 0.3438
+y_max = 512
+
+def printusage():
+    print ('''
+Takes a csv file with no header and three columns where each
+row[0] = x, row[1] = y and row[2] = z are coordinates of a point
+(in micrometers). Converts x, y to pixel coordinates and z to 
+z plane indices and creates a point ROI on the given image for
+each point. Adjust the x,y,z pixel sizes in the script!
+
+Usage: python create_rois.py [path to coordinates.csv] [Image ID]
+
+Environment variables OMERO_USER, OMERO_PASSWORD, OMERO_HOST and OMERO_PORT
+are not necessary but are taken into account if set.
+          ''')
+    sys.exit(1)
+
+
+if len(sys.argv) < 3:
+    printusage()
+else:
+    csv_file = sys.argv[1]
+    image_id = sys.argv[2]
+
+
+def add_point(us, img, x, y, z):
+  """ Adds a point ROI to the given image
+  Args:
+  us The UpdateService
+  img The image (ImageWrapper)
+  x x (in pixels)
+  y y (in pixels)
+  z z plane index
+  """
+  p = omero.model.PointI()
+  p.x = rdouble(x)
+  p.y = rdouble(y)
+  p.theZ = rint(z)
+  roi = omero.model.RoiI()
+  roi.setImage(img._obj)
+  roi.addShape(p)
+  us.saveAndReturnObject(roi)
+
+def calc_pos(x, y, z):
+  """ Calculates the position in pixels / z plane index
+  Args:
+  x x (in micrometers)
+  y y (in micrometers)
+  z z (in micrometers)
+
+  Returns:
+  x, y (pixels), z (z plane index) (tuple)
+  """
+  x_pix = x / x_size
+  if x_pix >= x_max:
+    raise Exception("x coordinate out of bounds")
+
+  y_pix = y / y_size
+  if y_pix >= y_max:
+    raise Exception("y coordinate out of bounds")
+
+  z_index = int(float(z) / z_size)
+  if z_index < 0 or z_index >= z_planes:
+    raise Exception("z_index out of bounds")
+
+  return x_pix, y_pix, z_index
+
+
+host = os.environ.get('OMERO_HOST', 'localhost')
+port = int(os.environ.get('OMERO_PORT', '4064'))
+
+conn = BlitzGateway(os.environ.get('OMERO_USER', 'public'),
+                    os.environ.get('OMERO_PASSWORD', 'public'),
+                    host=host, port=port)
+conn.connect()
+
+us = conn.getUpdateService()
+img = conn.getObject("Image", int(image_id))
+
+df = pandas.read_csv(csv_file, header=None)
+count = 0
+total = df.size / 3
+for index, row in df.iterrows():
+  x, y, z = calc_pos(row[0], row[1], row[2])
+  add_point(us, img, x, y, z)
+  count += 1
+  print("{}/{} done".format(count, total))
+
+conn.close()
+
